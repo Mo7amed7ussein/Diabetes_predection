2ec05ed254252771248fdd058e979d6771edaff6
Mo7amed7ussein <125216893+Mo7amed7ussein@users.noreply.github.com>
2023-10-22 21:08:04 +0300

    Update Data.py

--- Project/pages/Data.py
+++ Project/pages/Data.py
@@ -11,7 +11,7 @@ st.markdown(" <center>  <h1> Diabetes Dataset </h1> </font> </center> </h1> ",
 st.markdown('<a href="https://www.kaggle.com/datasets/iammustafatz/diabetes-prediction-dataset"> <center> Link to Dataset  </center> </a> ', unsafe_allow_html=True)
 
 # Load data
-df = pd.read_csv("../Project/Sources/Diabetes_clean.csv")
+df = pd.read_csv("../Project/sources/Diabetes_clean.csv")
 
 # Show data
 st.write(df.head())
