f6d109e6dc44004c1bc49cec94ee061499af7289
Mo7amed7ussein <125216893+Mo7amed7ussein@users.noreply.github.com>
2023-10-24 22:27:46 +0300

    Update Analysis.py

--- Project/pages/Analysis.py
+++ Project/pages/Analysis.py
@@ -25,7 +25,6 @@ with tab_univariate_analysis:
 
         elif option1 == 'Smoking history':
             st.subheader('b- Smoking history')
-            st.write('There is high correlation between diabetes infection and the two features [Blood glucose level, HbA1c level]')
             st.write('You can notice that the most values of the chart is "No Info".')
             st.plotly_chart(fy.fig2)
 
@@ -110,4 +109,5 @@ with tab_bivariate_analysis:
 
     else:
         st.subheader('9- Correlation')
+        st.write('There is high correlation between diabetes infection and the two features [Blood glucose level, HbA1c level]')
         st.plotly_chart(fy.fig19)
